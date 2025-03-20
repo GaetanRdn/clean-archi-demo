bd997c43b12ddff4bd1a65679547790f53ebb549
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2025-03-20 11:17:43 +0100

    feat: commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `<h1>{{ title }}</h1>`,
+  template: `<h1>{{ title }}</h1> `,
 })
 export class AppComponent {
-  title = 'Task list';
+  readonly title = 'Task list';
 }
